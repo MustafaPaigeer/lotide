666f40956eebd7190839354ac95d930607632ac5
Mustafa Paigeer <paigeer2012@gmail.com>
2022-02-09 23:26:40 +0000

    Adding completed  middle function

---
+++ middle.js
@@ -0,0 +1,33 @@
+const eqArrays = function(array1, array2) {
+  let result = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  let result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`Assertion Passed: ${result} ===  ${result}`);
+  } else {
+    console.log(`Assertion Failed: ${result} !== true`);
+  }
+};
+
+const middle = function(array) {
+  let arrayMiddle = Math.floor(array.length / 2);
+  if (array.length < 3) {
+    return [];
+  } else if (array.length % 2 !== 0) {
+    return [array[arrayMiddle]];
+  } else if (array.length % 2 === 0) {
+    return array.slice(arrayMiddle - 1 , arrayMiddle);
+  }
+};
+
+const testArray = ([2, 44, 33, 55, "apple", "banana", "blueberry", 100, 400, 'jj']);
+assertArraysEqual(middle(testArray), ["apple", "banana"]);
