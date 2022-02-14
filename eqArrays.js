49313f6da4a2cd40098fc39fad2d069567d9468c
Mustafa Paigeer <paigeer2012@gmail.com>
2022-02-14 21:06:54 +0000

    refactoring eqArrays.js and adding eqArraysTest file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   let result = true;
   for (let i = 0; i < array1.length; i++) {
@@ -17,4 +9,5 @@ const eqArrays = function(array1, array2) {
   return result;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+module.exports = eqArrays;
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,14 +1,11 @@
 const assertEqual = require('../assertEqual');
-const head = require('../head');
+
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
 
-// Test Code for HEAD file
 
-assertEqual(head([]), 5);
-assertEqual(head(["Hello"]), "Hello");
 
 
